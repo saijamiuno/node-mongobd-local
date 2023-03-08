cd8ab563dc31a8da21eae717f11b4db9f969ed57
saijami <saijami@unosimple.com>
2023-03-08 15:22:00 +0530

    get needed to change

--- notes-app/playground/testPost.js
+++ notes-app/playground/testPost.js
@@ -1,66 +1,58 @@
 const express = require("express");
+mongoose = require("mongoose");
 const MongoClient = require("mongodb").MongoClient;
 const mongodb = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
 
-mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
 const url = "mongodb://127.0.0.1:27017/";
 const dbName = "CRUD";
 
-async function getDocument(query) {
-  const client = await MongoClient.connect(url);
-  const db = client.db(dbName);
-  const collection = db.collection("dataJson");
+const query = {
+  _id: "6405e1019f98a127e6e3f837",
+};
 
-  const document = await collection.findOne(query);
-
-  client.close();
-
-  return document;
-}
-
-async function saveResponseToMongoDB(dataJson) {
-  const client = await MongoClient.connect("mongodb://127.0.0.1:27017/", {
-    useNewUrlParser: true,
-  });
-  const db = client.db("mongodbVSCodePlaygroundDB");
-
-  try {
-    const result = await db.collection("dataJson").insertOne(dataJson);
-    console.log(`Saved response with ID: ${result.insertedId}`);
-  } catch (err) {
-    console.error(`Error saving response: ${err}`);
-  } finally {
-    client.close();
-  }
-}
-
-app.post("/testDb", (req, res) => {
+app.post("/post", (req, res) => {
   console.log(req.body);
   let dataJson = JSON.parse(JSON.stringify(req.body));
   console.log(dataJson);
   saveResponseToMongoDB(dataJson);
   res.status(200).json(dataJson);
+
+  async function saveResponseToMongoDB(dataJson) {
+    const client = await MongoClient.connect("mongodb://127.0.0.1:27017/", {
+      useNewUrlParser: true,
+    });
+    const db = client.db("CRUD");
+
+    try {
+      const result = await db.collection("dataJson").insertOne(dataJson);
+      console.log(`Saved response with ID: ${result.insertedId}`);
+    } catch (err) {
+      console.error(`Error saving response: ${err}`);
+    } finally {
+      client.close();
+    }
+  }
 });
 
-// app.get("/get", () => {
-//   const result = db.collection("dataJson").find();
-//   console.log({ result: result });
+app.get("/get", (req, res) => {
+  res.status(200).send(getDocument());
 
-// });
+  async function getDocument(CRUD, query) {
+    const client = await db.dataJson.find({
+      _id: "6405e1019f98a127e6e3f837",
+    });
+    const db = client.db(dbName);
+    const collection = db.collection("dataJson");
+    const documents = await collection.find(query).toArray();
+    // client.close();
 
-app.get("/get",(req,res)  => {
-  getDocument("6405e1019f98a127e6e3f837")
-  
+    return documents;
+  }
 });
 
 app.listen(3006, () => {
   console.log("Server is running on port 3006");
 });
-
-// Connection URL and database name
-
-// Function to retrieve a document from a collection
